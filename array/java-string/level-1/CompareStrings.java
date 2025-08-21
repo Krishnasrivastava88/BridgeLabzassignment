32407834a1c839f506701df80665bfcd9dfc8476
Krishnasrivastava88 <krissh8858@gmail.com>
2025-08-21 16:33:33 +0530

    Added CompareStrings.java program in level-1

---
+++ array/java-string/level-1/CompareStrings.java
@@ -0,0 +1,12 @@
+public class CompareStrings {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "World";
+        
+        if(s1.equals(s2)) {
+            System.out.println("Strings are equal");
+        } else {
+            System.out.println("Strings are not equal");
+        }
+    }
+}
